6356fd7b18585bf84b58e6e6181f56d9b96cd7cd
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2018-01-17 14:59:04 +0100

    Init shell

---
+++ Gruntfile.js
@@ -0,0 +1,420 @@
+module.exports = function(grunt) {
+	/**
+	 * @typedef {function} grunt.option
+	 * @typedef {function} grunt.initConfig
+	 */
+	
+	/** possible build :
+	 *
+	 * > build files to prepare pushing on App Script
+	 * grunt build --target=dev
+	 * grunt build --target=prod
+	 * 
+	 * grunt publishAddon --target=prod
+	 */
+	
+	// define target and config to run the task with
+	const TARGET = grunt.option('target') || 'dev';
+	
+	/**
+	 * Load config file directly
+	 * 
+	 * @type {{
+	 *   clasp: {
+	 *     scriptId: string
+	 *   },
+	 *   publishing: {
+	 *     version: number,
+	 *     versionOffset: number,
+	 *     account: string,
+	 *     appID: string
+	 *   },
+	 *   manifest: Object
+	 * }}
+	 */
+	const CONFIG = grunt.file.readJSON(`./build/config/${TARGET}_config.json`);
+	
+	
+	// init config object
+	grunt.initConfig({
+		// get JSON config object
+		// pkg: grunt.file.readJSON('package.json'),
+		
+		// task
+		clean: {
+			'build': {
+				files: [
+					{
+						expand: true,
+						cwd: 'build/src/', // build src folder
+						src: [
+							'*'
+						]
+					}
+				]
+			}
+		},
+		copy:{
+			'build': {
+				files: [
+					{
+						expand: true,
+						cwd: 'src/',
+						src: '**/*.gs.js',
+						dest: 'build/src/',
+						flatten: true,
+						filter: 'isFile',
+						rename: function (dest, src) {
+							return dest + src.replace(/\.gs\.js$/, '.gs');
+						}
+					},
+					{
+						expand: true,
+						cwd: 'src/',
+						src: '**/*.js',
+						dest: 'build/src/',
+						flatten: true,
+						filter: 'isFile',
+						rename: function (dest, src) {
+							return dest + src.replace(/\.js$/, '.gs');
+						}
+					},
+					{
+						expand: true,
+						cwd: 'src/',
+						src: ['**/*.html', '**/*.json'],
+						dest: 'build/src/',
+						flatten: true,
+						filter: 'isFile'
+					}]
+			}
+		},
+		config: {
+			'clasp': {
+				src: "build/src/.clasp.json",
+				dest: "build/src/.clasp.json",
+				configuration: CONFIG.clasp
+			}
+		},
+		clasp: {
+			'newVersion': {
+				runDir: 'build/src',
+			},
+			'push': {
+				runDir: 'build/src',
+			},
+		},
+		zip: {
+			manifest: {}
+		},
+		webstore:{
+			'updateItem': {
+				/** Do publish or just update the draft */
+				publish: true,
+				
+				/** @type {'trustedTesters' | 'default'} */
+				target: 'trustedTesters'
+			}
+		}
+	});
+	
+	
+	// load tasks
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	
+	// custom task to change *.json
+	grunt.registerMultiTask('config', 'Update properties in *.json file', function(){
+		
+		let self = this;
+		if (!self || !self.data || !self.data.src || !self.data.dest) return;
+		
+		let configFile = self.files[0].src[0],
+			configFileTarget = self.files[0].dest;
+		
+		let newConfig = self.data.configuration,
+			config;
+		try {
+			// read config file
+			config = grunt.file.readJSON(configFile)
+		}
+		catch (e) {
+			config = {};
+		}
+		
+		// update the provided parameters
+		for (let i in newConfig){
+			// noinspection JSUnfilteredForInLoop
+			config[i] = newConfig[i];
+		}
+		
+		// write updated config
+		let configStr = JSON.stringify(config);
+		grunt.file.write(configFileTarget, configStr);
+		
+	});
+	
+	// Use clasp
+	grunt.registerMultiTask('clasp', 'push content in script, and create a version', function(){
+		const child_process = require('child_process');
+		
+		/**
+		 * @type {{
+		 *   command: string,
+		 *   runDir: string
+		 * }}
+		 */
+		let param = this.data;
+		
+		function clasp(cmd){
+			let res = child_process.execSync(`clasp ${cmd}`, {
+				cwd: __dirname +'/'+ param.runDir
+			});
+			
+			// Get string res
+			return res.toString();
+		}
+		
+		switch (this.target){
+			case 'push':
+				// Push
+				console.log('Pushing files to the script');
+				let pushRes = clasp('push');
+				
+				// Check result
+				let resPush = /Pushed\s(\d+)\sfiles\./.exec(pushRes);
+				if (!resPush) throw 'Error while pushing files to AppsScript';
+				console.log(`Pushed files: ${resPush[1]}`);
+				
+				break;
+			
+			case 'newVersion':
+				// create a new version
+				console.log('Creating new script version');
+				let versionRes = clasp('version');
+				
+				// Check result and get version num
+				let resVers = /version\s(\d+)/.exec(versionRes);
+				if (!resVers) throw 'Error while creating new version';
+				
+				let versionNum = +resVers[1];
+				console.log('New version num: ' + versionNum);
+				
+				// Update version value:
+				CONFIG.publishing.version = versionNum;
+				
+				break;
+		}
+	});
+	// Update addon ZIP file
+	grunt.registerMultiTask('zip', 'Update addon zip file to prepare it for webstore deploy', function(){
+		const JSZip = require('jszip');
+		const fs = require('fs');
+		let done = this.async();
+		
+		let manifest = CONFIG.publishing.manifest;
+		
+		if (!CONFIG.publishing.version){
+			console.error(`The webstore draft can't be created with no script version`);
+			
+			return false;
+		}
+		
+		// Update script version
+		manifest['container_info'].container_version = `${CONFIG.publishing.version}`;
+		// Bump manifest version (as script are always versioned, this should suffice)
+		manifest.version = `${CONFIG.publishing.versionOffset + CONFIG.publishing.version}`;
+		
+		
+		let zip = new JSZip();
+		/**
+		 * @typedef {function} zip.file
+		 */
+		
+		new Promise((resolve, reject) => {
+			fs.readFile('build/addon_webstore.zip', (err, data) => {
+				if (err) reject(err);
+				
+				resolve(data);
+			});
+		})
+			.then(data => zip.loadAsync(data))
+				
+			// Update manifest.json & create zip
+			.then(() => {
+				// Update manifest.json
+				return zip.file('manifest.json', JSON.stringify(manifest))
+				// create Zip file
+					.generateAsync({type:"nodebuffer"})
+			})
+			
+			// Write to disk
+			.then(content => new Promise(resolve => {
+				fs.writeFile("build/addon_webstore_draft.zip", content, resolve);
+			}))
+			
+			// end task
+			.then(err => {
+				if (err) throw err;
+				
+				console.log('file create with success');
+				
+				done();
+			})
+			.catch(err => {
+				console.error(err);
+				
+				done(false);
+			})
+		
+	});
+	// Use webStore API
+	grunt.registerMultiTask('webstore', 'use Webstore API to send a draft and publish it', function(){
+	
+		// If we need to publish to unlisted, we need to update 
+		/*
+	# In case of 'unlisted' target, only passing 'publishTarget' as a request header works
+		if [[ ${PUBLISH_TARGET} == 'unlisted' ]]; then
+		HTTP_CODE=$(curl \
+        -w %{http_code} \
+        -o api-response.json \
+        -H "Authorization: Bearer $ACCESS_TOKEN"  \
+        -H "x-goog-api-version: 2" \
+        -H "Content-Length: 0" \
+        -H "publishTarget: $PUBLISH_TARGET" \
+        -X POST \
+        -v https://www.googleapis.com/chromewebstore/v1.1/items/${EXTENSION_ID}/publish)
+			# For other targets ('default' or 'trustedTesters') we must pass 'target' as request body in json format
+	else
+		HTTP_CODE=$(curl \
+        -w %{http_code} \
+        -o api-response.json \
+        -H "Authorization: Bearer $ACCESS_TOKEN" \
+        -H "x-goog-api-version: 2" \
+        -H "Content-Type: application/json" \
+        -d "{\"target\":\"$PUBLISH_TARGET\"}" \
+        -X POST \
+        -v https://www.googleapis.com/chromewebstore/v1.1/items/${EXTENSION_ID}/publish)
+	fi
+		*/
+		
+		
+		const webstore = require('webstore-upload');
+		
+		const PUBLISH_DRAFT = this.data.publish || false;
+		const PUBLICATION_TARGET = this.data.target || 'trustedTesters' ;
+		
+		
+		if (!CONFIG.publishing.account){
+			console.error('No publishing account, please update the configuration');
+			return false;
+		}
+		
+		
+		// Load credentials
+		const credentials = require('./build/cred/client_secret');
+		const accountCred = credentials[CONFIG.publishing.account];
+		
+		if (!accountCred){
+			console.error('Publishing account not found in the credential file');
+			return false;
+		}
+		
+		
+		const uploadOptions = {
+			accounts: {
+				default: { //account under this section will be used by default 
+					//publish: true, //publish item right after uploading. default false 
+					client_id: accountCred['installed'].client_id,
+					client_secret: accountCred['installed'].client_secret,
+					refresh_token: accountCred.refresh_token
+				},
+				/*other_account: {
+				 publish: true, //publish item right after uploading. default false 
+				 client_id: 'ie204es2mninvnb.apps.googleusercontent.com',
+				 client_secret: 'LEJDeBHfS',
+				 refresh_token: '1/eeeeeeeeeeeeeeeeeeeeeee_aaaaaaaaaaaaaaaaaaa'
+				 },
+				 new_account: {
+				 cli_auth: true, // Use server-less cli prompt go get access token. Default false 
+				 publish: true, //publish item right after uploading. default false 
+				 client_id: 'kie204es2mninvnb.apps.googleusercontent.com',
+				 client_secret: 'EbDeHfShcj'
+				 }*/
+			},
+			extensions: {
+				addon: {
+					//required
+					appID: CONFIG.publishing.appID,
+					//required, we can use dir name and upload most recent zip file 
+					zip: 'build/addon_webstore_draft.zip',
+					publishTarget: PUBLICATION_TARGET,
+					publish: PUBLISH_DRAFT,
+				},
+				/*extension2: {
+				 account: 'new_account',
+				 //will rewrite values from 'account' section 
+				 publish: true,
+				 appID: 'jcbeonnlikcefedeaijjln',
+				 zip: 'test/files/test2.zip',
+				 publishTarget: 'trustedTesters'
+				 }*/
+			},
+			uploadExtensions : ['addon']
+		};
+		
+		let done = this.async();
+		
+		webstore(uploadOptions, 'default')['then'](() => {
+			console.log('Published with success');
+			
+			done();
+		})
+			.catch(() => {
+				console.error('Publishing failed');
+				
+				done(false);
+			});
+	});
+	
+	
+	// NEVER name the task as the configurator object
+	// the default task can be run just by typing "grunt" on the command line
+	// USE THIS when multiple tasks must be chained
+	grunt.registerTask('build', [
+		'clean:build',
+		'copy:build',
+		'config:clasp'
+	]);
+	
+	grunt.registerTask('push', [
+		'build',
+		'clasp:push',
+	]);
+	
+	grunt.registerTask('publishAddon', [
+		'clasp:push',
+		'clasp:newVersion',
+		'zip:manifest',
+		'webstore:updateItem'
+	]);
+	
+	grunt.registerTask('build_push', [
+		'build',
+		'push',
+	]);
+	grunt.registerTask('build_publish', [
+		'build',
+		'publishAddon',
+	]);
+	
+	// define default task (for grunt alone)
+	grunt.registerTask('default', ['build']);
+	
+	/**
+	 * NOTES:
+	 *
+	 * - Beware of comments in JSON files, no comments can exists in JSON processed by config task
+	 *
+	 */
+};
---
+++ README.md
@@ -0,0 +1,141 @@
+# README
+
+## How do I get set up?
+
+#### Summary of set up
+
+1. We assume that a code Editor (Webstorm, Visual Studio Code -> free), Git, and npm are installed on you computer.
+2. Clone the Git repository
+3. Run npm install in project root to install **_node_modules_** necessary to run grunt and build the project
+4. Create a _**/build/config/dev_config.json**_ file
+It will contain the built target, and must be of the following form:
+```
+{
+	"clasp": {"scriptId": "AppsScript file drive ID"},
+	"publishing": {}
+}
+```
+5. We use the **@google/clasp** package to get/update apps script file in Google Drive.
+Find the setup for clasp on this page: https://www.npmjs.com/package/@google/clasp
+
+
+#### Build command
+
+1. Renaming and copy files: Execute ``grunt build --target=dev`` inside the main working directory
+2. Update the script: Execute ``grunt push --target=dev``
+3. Both can be executed with: ``grunt build_push --target=dev``
+4. To publish an addon (JUST FOR TEST): ``grunt build_publish --target=testAddon``
+
+#### Changing target (DEV, PROD, testAddon for example)
+
+You can define other target by using the "--target=targetName" grunt option.
+For each target, a valid config file named: "/build/config/targetName_config.json" must exist.
+
+#### Config file format:
+
+When not publishing as an Addon:
+
+/build/config/$CONFiG_NAME$_config.json
+```json
+{
+	"clasp": {
+		"scriptId": "$TARGET_SCRIPT_ID$"
+	},
+	"publishing": {}
+}
+```
+
+When publishing as an Addon with the command:
+
+/build/config/$CONFiG_NAME$_config.json
+```json
+{
+	"clasp": {
+		"scriptId": "$TARGET_SCRIPT_ID$"
+	},
+	"publishing": {
+		"version": null,
+		"versionOffset": 0,
+		"account": "$ACCOUNT_NAME_FOR_WEBSTORE_CREDENTIAL$",
+		"appID": "$WEBSTORE_ITEM_ID$",
+		
+		"manifest": {
+			"update_url" : "https://clients2.google.com/service/update2/crx",
+			"container" : ["GOOGLE_SPREADSHEET"],
+			"app" : {
+				"background" : {
+					"scripts" : ["background.js"]
+				}
+			},
+			"api_console_project_id" : "$SCRIPT_PROJECT_ID$",
+			"container_info" : {
+				"post_install_tip" : "$POST_INSTAL_TEXT$",
+				"container_id" : "$OLD_SCRIPT_ID$",
+				"container_version" : "0"
+			},
+			"manifest_version" : 2,
+			"name" : "$ADDON_NAME$",
+			"icons" : {
+				"16" : "script-icon_16.png",
+				"128" : "script-icon_128.png"
+			},
+			"version" : "0"
+		}
+	}
+}
+```
+NOTE: The "manifest" key can be copied from the chrome webstore item, all field will already be correctly filled.
+
+NOTE: The first publishing must be done manually.
+
+#### Credential file format:
+
+Those credentials can be obtained by creating a Google project on the addon used to publish the Addon.
+Then following the instructions here: https://developer.chrome.com/webstore/using_webstore_api#beforeyoubegin
+Download the json file for the created ClientID and use it to fill the credential file below.
+
+More information on the webstore API used: https://www.npmjs.com/package/webstore-upload
+
+/build/config/$CONFiG_NAME$_config.json
+```json
+{
+	"$ACCOUNT_NAME_FOR_WEBSTORE_CREDENTIAL$": {
+		"refresh_token": "$REFRESH_TOKEN$",
+		"installed": {
+			"client_id": "$CLIENT_ID$",
+			"client_secret": "$CLIENT_SECRET$"
+		}
+	},
+	"$ANOTHER_ACCOUNT_NAME_FOR_WEBSTORE_CREDENTIAL$": "{...}"
+}
+```
+NOTE: The first code obtain when executing the procedure to obtain the refresh_token is NOT the refresh_token, it's a authorizatin code (starting by "4/")
+
+The refresh_token usualy starts by "1/" and is obtained by exchanging the auth code. All the procedure on this page must be followed until the end: https://developer.chrome.com/webstore/using_webstore_api#beforeyoubegin
+
+#### Test instructions
+
+1. Publish -> Test as add-on (non-triggerable functionalities)
+2. Publish -> Publish as add-on with permission to people with link (for triggable functionalities)
+
+## Best practices ###
+
+Git branches:
+
+* master: merge Pull resquests on master
+
+* Set **tags** with increasing version on master's commit that are pushed in production (v1, v2, ...)
+
+**Always merge with no fast forward !** (For major feature and update. Quick fix can be merged by default):
+```
+git merge --no-ff branchToMergeHere
+```
+
+## WARNING ###
+
+Using the command to publish as an addon will **FORBID** further manual publishing with the GAS interface.
+
+## Who do I talk to? ###
+
+* jean-remi.delteil@revevol.eu
+* romain.vialard@revevol.eu
