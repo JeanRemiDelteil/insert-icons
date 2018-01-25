82f9142ff110d72113adf5f240f7ea18655763ba
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2018-01-25 18:56:16 +0100

    update google.script.run with latest version (got working auto-complete)

--- google.script.run.js
+++ google.script.run.js
@@ -5,15 +5,11 @@
  */
 
 
-let google = {
-	script: {
-		run: function () {}
-	}
-};
+//<editor-fold desc="# Stubs for google.script.run">
 
-// NEVER DO THAT in normal code
-// this overwrite the prototype, here we do this of auto-completion only
-google.script.run.prototype = {
+GoogleScriptRun = function () {};
+// NEVER DO THAT in normal code, this overwrite the prototype, here we do this of auto-completion only
+GoogleScriptRun.prototype = {
   onInstall: onInstall,
   onOpen: onOpen,
 	
@@ -21,20 +17,35 @@ google.script.run.prototype = {
 };
 
 
+//<editor-fold desc="# Internal stubs for google.script.run">
+
 /**
  * @param {function} successHandler
- * @return {google.script.run}
+ * @return {GoogleScriptRun}
  */
-google.script.run.withSuccessHandler = function (successHandler) {};
+GoogleScriptRun.prototype.withSuccessHandler = function (successHandler) {
+  successHandler();
+  return this;
+};
 /**
  * @param {function} failureHandler
- * @return {google.script.run}
+ * @return {GoogleScriptRun}
  */
-google.script.run.withFailureHandler = function (failureHandler) {};
+GoogleScriptRun.prototype.withFailureHandler = function (failureHandler) {
+  failureHandler();
+  return this;
+};
 
-google.script.run.prototype.withSuccessHandler = google.script.run.withSuccessHandler;
-google.script.run.prototype.withFailureHandler = google.script.run.withFailureHandler;
+// noinspection ES6ConvertVarToLetConst
+var google = {
+  script: {
+    run: new GoogleScriptRun()
+  }
+};
 
+//</editor-fold>
+
+//</editor-fold>
 
 
 //<editor-fold desc="# Stubs for Google API">
@@ -274,4 +285,3 @@ google.script.run.prototype.withFailureHandler = google.script.run.withFailureHa
 //</editor-fold>
 
 //</editor-fold>
-
